1c604e8fab4f566c1d4f556844d2ee4c5515f768
reefuu <rifqiet@gmail.com>
2023-10-18 15:43:04 +0700

    kevinn

--- app/src/main/java/com/example/testkevin/MainActivity.kt
+++ app/src/main/java/com/example/testkevin/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android kevin halooooo aku kevin android")
+                    Greeting("Android p info kebin")
                 }
             }
         }
