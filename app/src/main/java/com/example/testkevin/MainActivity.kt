22db5a5fb80528d57ce5a3b24aa45158519e5e57
reefuu <rifqiet@gmail.com>
2023-10-18 15:38:41 +0700

    studioabcdefasd

--- app/src/main/java/com/example/testkevin/MainActivity.kt
+++ app/src/main/java/com/example/testkevin/MainActivity.kt
@@ -39,6 +39,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
