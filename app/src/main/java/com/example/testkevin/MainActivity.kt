8282f9f377e51ec1d37114213d9b0b57069c87aa
kevin <kchristian03@student.ciputra.ac.id>
2023-10-18 13:49:28 +0700

    init

--- app/src/main/java/com/example/testkevin/MainActivity.kt
+++ app/src/main/java/com/example/testkevin/MainActivity.kt
@@ -35,6 +35,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    Text(
+        text = "Hello rif!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
